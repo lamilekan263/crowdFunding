e6c33c5b5241ac141b8e8a8b951e4af5df4c67a8
lamilekan263 <lamilekan263@gmail.com>
2023-07-04 18:52:39 +0100

    setup component and hardhat configurations

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 
+// nterbal
+import {Navbar, Footer} from '../Components'
+
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component { ...pageProps } />
+      <Footer/>
+    </>
+  )
 }
 
 export default MyApp
